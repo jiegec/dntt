33c6606a8a476cfb3632d64061428e4cf38a9dce
Jiajie Chen <c@jia.je>
2023-06-22 08:26:40 +0800

    Support mellanox switch

--- dntt/dump.py
+++ dntt/dump.py
@@ -6,6 +6,7 @@ with open('config.yaml', 'r') as f:
     config = yaml.load(f, Loader=yaml.Loader)
     for switch in config['switches']:
         with Telnet(switch['host']) as tn:
+            tn.set_debuglevel(100)
             if switch['vendor'] == 'huawei':
                 tn.write(f'{switch["username"]}\n'.encode('utf-8'))
                 tn.write(f'{switch["password"]}\n'.encode('utf-8'))
@@ -17,14 +18,27 @@ with open('config.yaml', 'r') as f:
                 output = output[output.find('display current-configuration'):]
                 output = "\n".join(output.split("\n")[1:-1])
             elif switch['vendor'] == 'dell':
-                tn.set_debuglevel(100)
                 tn.write(f'{switch["username"]}\n'.encode('utf-8'))
+                tn.read_until(f'Password:'.encode('utf-8'))
                 tn.write(f'{switch["password"]}\n'.encode('utf-8'))
                 tn.write(f'en\n'.encode('utf-8'))
                 tn.write(f'terminal length 0\n'.encode('utf-8'))
                 tn.write(f'show run\n'.encode('utf-8'))
                 tn.write(f'exit\n'.encode('utf-8'))
                 output = tn.read_until('#exit'.encode('utf-8')).decode('utf-8')
+                # strip additional content
+                output = output[output.find('show run'):]
+                output = "\n".join(output.split("\n")[1:-1])
+            elif switch['vendor'] == 'mellanox':
+                tn.read_until(f'login:'.encode('utf-8'))
+                tn.write(f'{switch["username"]}\n'.encode('utf-8'))
+                tn.read_until(f'Password:'.encode('utf-8'))
+                tn.write(f'{switch["password"]}\n'.encode('utf-8'))
+                tn.write(f'en\n'.encode('utf-8'))
+                tn.write(f'no cli session paging enable\n'.encode('utf-8'))
+                tn.write(f'show run\n'.encode('utf-8'))
+                tn.write(f'exit\n'.encode('utf-8'))
+                output = tn.read_until('# exit'.encode('utf-8')).decode('utf-8')
                 # strip additional content
                 output = output[output.find('show run'):]
                 output = "\n".join(output.split("\n")[1:-1])
