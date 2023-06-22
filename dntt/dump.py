0ca1344620bad2b94cc3730f4b19312a91136a8f
Jiajie Chen <c@jia.je>
2023-06-22 16:12:30 +0800

    Use SSH instead of telnet

--- dntt/dump.py
+++ dntt/dump.py
@@ -1,65 +1,71 @@
 import yaml
 import re
-from telnetlib import Telnet
+from pexpect import pxssh
+import pexpect
+import sys
 
 with open('config.yaml', 'r') as f:
     config = yaml.load(f, Loader=yaml.Loader)
     for switch in config['switches']:
-        with Telnet(switch['host']) as tn:
-            tn.set_debuglevel(100)
-            if switch['vendor'] == 'huawei':
-                tn.read_until(f'Username:'.encode('utf-8'))
-                tn.write(f'{switch["username"]}\n'.encode('utf-8'))
-                tn.read_until(f'Password:'.encode('utf-8'))
-                tn.write(f'{switch["password"]}\n'.encode('utf-8'))
-                tn.write(f'screen-length 0 temporary\n'.encode('utf-8'))
-                tn.write(f'display current-configuration\n'.encode('utf-8'))
-                tn.write(f'quit\n'.encode('utf-8'))
-                output = tn.read_until('quit'.encode('utf-8')).decode('utf-8')
-                # strip additional content
-                output = output[output.find('display current-configuration'):]
-                output = "\n".join(output.split("\n")[1:-1])
-            elif switch['vendor'] == 'dell':
-                tn.write(f'{switch["username"]}\n'.encode('utf-8'))
-                tn.read_until(f'Password:'.encode('utf-8'))
-                tn.write(f'{switch["password"]}\n'.encode('utf-8'))
-                tn.write(f'en\n'.encode('utf-8'))
-                tn.write(f'terminal length 0\n'.encode('utf-8'))
-                tn.write(f'show run\n'.encode('utf-8'))
-                tn.write(f'exit\n'.encode('utf-8'))
-                output = tn.read_until('#exit'.encode('utf-8')).decode('utf-8')
-                # strip additional content
-                output = output[output.find('show run'):]
-                output = "\n".join(output.split("\n")[1:-1])
-            elif switch['vendor'] == 'mellanox':
-                tn.read_until(f'login:'.encode('utf-8'))
-                tn.write(f'{switch["username"]}\n'.encode('utf-8'))
-                tn.read_until(f'Password:'.encode('utf-8'))
-                tn.write(f'{switch["password"]}\n'.encode('utf-8'))
-                tn.write(f'en\n'.encode('utf-8'))
-                tn.write(f'no cli session paging enable\n'.encode('utf-8'))
-                tn.write(f'show run\n'.encode('utf-8'))
-                tn.write(f'exit\n'.encode('utf-8'))
-                output = tn.read_until('# exit'.encode('utf-8')).decode('utf-8')
-                # strip additional content
-                output = output[output.find('show run'):]
-                output = re.sub('## Generated at .*', '', output)
-                output = "\n".join(output.split("\n")[1:-1])
-            elif switch['vendor'] == 'cisco':
-                tn.read_until(f'login:'.encode('utf-8'))
-                tn.write(f'{switch["username"]}\n'.encode('utf-8'))
-                tn.read_until(f'Password:'.encode('utf-8'))
-                tn.write(f'{switch["password"]}\n'.encode('utf-8'))
-                tn.write(f'terminal length 0\n'.encode('utf-8'))
-                tn.write(f'show run\n'.encode('utf-8'))
-                tn.write(f'exit\n'.encode('utf-8'))
-                output = tn.read_until('# exit'.encode('utf-8')).decode('utf-8')
-                # strip additional content
-                output = output[output.find('show run'):]
-                output = re.sub('!Time: .*', '', output)
-                output = "\n".join(output.split("\n")[1:-1])
-            else:
-                continue
+        child = pexpect.spawn(
+            f'ssh -oHostKeyAlgorithms=+ssh-rsa,ssh-dss -o KexAlgorithms=+diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1 {switch["username"]}@{switch["host"]}', encoding='utf-8')
+        child.logfile = sys.stdout
+        if switch['vendor'] == 'huawei':
+            child.expect('Password:')
+            child.sendline(switch["password"])
+            child.expect('>')
+            child.sendline('screen-length 0 temporary')
+            child.sendline('display current-configuration')
+            child.sendline('quit')
+            child.expect('quit')
+            output = child.before
+            # strip additional content
+            output = output[output.find('display current-configuration'):]
+            output = "\n".join(output.split("\n")[1:-1])
+        elif switch['vendor'] == 'dell':
+            child.expect('password:')
+            child.sendline(switch["password"])
+            child.expect('>')
+            child.sendline('en')
+            child.sendline('terminal length 0')
+            child.sendline('show run')
+            child.sendline('exit')
+            child.expect('#exit')
+            output = child.before
+            # strip additional content
+            output = output[output.find('show run'):]
+            output = "\n".join(output.split("\n")[1:-1])
+        elif switch['vendor'] == 'mellanox':
+            child.expect('Password:')
+            child.sendline(switch["password"])
+            child.expect('>')
+            child.sendline('en')
+            child.sendline('no cli session paging enable')
+            child.sendline('show run')
+            child.sendline('exit')
+            child.expect('# exit')
+            output = child.before
+            # strip additional content
+            output = output[output.find('show run'):]
+            output = output[output.find('exit'):]
+            output = re.sub('## Generated at .*', '', output)
+            output = "\n".join(output.split("\n")[1:-1])
+        elif switch['vendor'] == 'cisco':
+            child.expect('Password:')
+            child.sendline(switch["password"])
+            child.expect('#')
+            child.sendline('terminal length 0')
+            child.sendline('show run')
+            child.sendline('exit')
+            child.expect('# exit')
+            output = child.before
+            # strip additional content
+            output = output[output.find('!Command: show running-config'):]
+            output = re.sub('!Time: .*', '', output)
+            output = "\n".join(output.split("\n")[1:-1])
+        else:
+            continue
+        child.close()
 
-            with open(f'{switch["host"]}.config', 'w') as cfg:
-                cfg.write(output)
+        with open(f'{switch["host"]}.config', 'w') as cfg:
+            cfg.write(output)
